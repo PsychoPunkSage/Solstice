c07939e48e86f8a39c20fdb61996ad8d1a4dc8d9
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-12-26 12:51:30 +0530

    fix::> READERS fn OKK
    Signed-off-by: Abhinav Prakash <abhinav.prakash319@gmail.com>

--- 1PriorityRnW/PriorityRnW/src/main2.rs
+++ 1PriorityRnW/PriorityRnW/src/main2.rs
@@ -5,12 +5,13 @@ use of `pthreads` directly in Rust is generally not recommended in Rust because:
     - Will lose Rust's safety guarantees and ownership model benefits
     - It's more error-prone than using Rust's standard library abstractions
 */
-use libc::{c_void, pthread_create, pthread_join, pthread_t};
+use libc::{c_void, pthread_create, pthread_join, pthread_t, usleep};
 use libc::{
     pthread_cond_broadcast, pthread_cond_destroy, pthread_cond_init, pthread_cond_t,
     pthread_cond_wait, pthread_mutex_destroy, pthread_mutex_init, pthread_mutex_lock,
     pthread_mutex_t, pthread_mutex_unlock, PTHREAD_COND_INITIALIZER, PTHREAD_MUTEX_INITIALIZER,
 };
+use rand::Rng;
 use std::ptr::null_mut;
 
 static mut SV: i32 = 0;
@@ -21,7 +22,35 @@ static mut MUTEX: pthread_mutex_t = PTHREAD_MUTEX_INITIALIZER;
 static mut CV_READER: pthread_cond_t = PTHREAD_COND_INITIALIZER;
 static mut CV_WRITER: pthread_cond_t = PTHREAD_COND_INITIALIZER;
 
-unsafe extern "C" fn readers(_arg: *mut c_void) /*-> *mut c_void*/ {}
+unsafe extern "C" fn readers(_arg: *mut c_void) -> *mut c_void {
+    let mut rng = rand::thread_rng();
+
+    (0..20).into_iter().for_each(|i| {
+        usleep(rng.gen_range(0..100000));
+
+        pthread_mutex_lock(&mut MUTEX);
+
+        // If writers is executing then WAIT
+        if C_WRITERS > 0 {
+            pthread_cond_wait(&mut CV_READER, &mut MUTEX);
+        }
+
+        C_READERS += 1;
+        println!("VAR(R): {}", SV);
+        println!("    |_> Current Readers: {}", C_READERS);
+        println!("    |_> Current Writers: {}", C_WRITERS);
+        C_READERS -= 1;
+
+        if C_READERS == 0 {
+            pthread_cond_broadcast(&mut CV_WRITER);
+        }
+
+        pthread_mutex_unlock(&mut MUTEX);
+    });
+
+    println!("Reader is Exiting");
+    null_mut()
+}
 unsafe extern "C" fn writers(_arg: *mut c_void) /*-> *mut c_void*/ {}
 
 fn main() {
