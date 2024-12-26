6b0c72913f54ee16e945d44df28126131d8776aa
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-12-26 11:39:29 +0530

    fix::> writer+reader impl ready
    Signed-off-by: Abhinav Prakash <abhinav.prakash319@gmail.com>

--- 1PriorityRnW/PriorityRnW/src/main.rs
+++ 1PriorityRnW/PriorityRnW/src/main.rs
@@ -1,7 +1,8 @@
+use rand::Rng;
 use std::{
-    io::stdin,
     sync::{Arc, Condvar, Mutex},
     thread,
+    time::Duration,
 };
 
 struct SharedState {
@@ -10,8 +11,65 @@ struct SharedState {
     writers_count: i32,
 }
 
-fn reader(state: Arc<(Mutex<SharedState>, Condvar, Condvar)>) {}
-fn writer(state: Arc<(Mutex<SharedState>, Condvar, Condvar)>) {}
+fn reader(state: Arc<(Mutex<SharedState>, Condvar, Condvar)>) {
+    let mut rng = rand::thread_rng();
+    (0..20).into_iter().for_each(|_| {
+        thread::sleep(Duration::from_millis(rng.gen_range(1..100))); // Random Sleep
+
+        let (lock, reader_cv, writer_cv) = &*state;
+        let mut guard = lock.lock().unwrap();
+
+        // WAIT if there are writers
+        while guard.writers_count > 0 {
+            guard = reader_cv.wait(guard).unwrap();
+        }
+
+        guard.readers_count += 1;
+
+        println!("VAR(R): {}", guard.shared_val);
+        println!("    |_> Current Readers: {}", guard.readers_count);
+        println!("    |_> Current Writers: {}", guard.writers_count);
+
+        guard.readers_count -= 1;
+
+        if guard.readers_count == 0 {
+            // Broadcast....
+            writer_cv.notify_all();
+        }
+    });
+
+    println!("Reader is Exiting");
+}
+fn writer(state: Arc<(Mutex<SharedState>, Condvar, Condvar)>) {
+    let mut rng = rand::thread_rng();
+
+    (1000..1020).into_iter().for_each(|i| {
+        thread::sleep(Duration::from_millis(rng.gen_range(1..100))); // Random Sleep
+
+        let (lock, reader_cv, writer_cv) = &*state;
+        let mut guard = lock.lock().unwrap();
+
+        guard.writers_count += 1;
+
+        // WAIT if there are Readers...
+        while guard.readers_count > 0 {
+            guard = writer_cv.wait(guard).unwrap();
+        }
+
+        guard.shared_val = i;
+
+        println!("VAR(W): {}", guard.shared_val);
+        println!("    |_> Current Readers: {}", guard.readers_count);
+        println!("    |_> Current Writers: {}", guard.writers_count);
+
+        guard.writers_count -= 1;
+
+        reader_cv.notify_all();
+        writer_cv.notify_all();
+    });
+
+    println!("Writer is Exiting");
+}
 
 fn main() {
     println!("Enter no. of Readers:");
@@ -24,23 +82,29 @@ fn main() {
     std::io::stdin().read_line(&mut input).unwrap();
     let n_writers: i32 = input.trim().parse().unwrap();
 
-    // STATE Init
+    ////////////////
+    // STATE Init //
+    ////////////////
     let state = Arc::new((
         Mutex::new(SharedState {
             shared_val: 0,
             readers_count: 0,
             writers_count: 0,
         }),
-        Condvar::new(),
-        Condvar::new(),
+        Condvar::new(), // reader cv
+        Condvar::new(), // writer cv
     ));
 
-    // Thread Handlers
+    /////////////////////
+    // Thread Handlers //
+    /////////////////////
     let mut readers = Vec::new();
     let mut writers = Vec::new();
 
-    // THREAD CREATION
-    (0..n_readers).into_iter().for_each(|i| {
+    /////////////////////
+    // THREAD CREATION //
+    /////////////////////
+    (0..n_readers).into_iter().for_each(|_| {
         let c_state = Arc::clone(&state);
         let handle = thread::spawn(move || {
             reader(c_state);
@@ -49,7 +113,7 @@ fn main() {
         readers.push(handle);
     });
 
-    (0..n_writers).into_iter().for_each(|i| {
+    (0..n_writers).into_iter().for_each(|_| {
         let c_state = Arc::clone(&state);
         let handle = thread::spawn(move || {
             writer(c_state);
@@ -57,7 +121,9 @@ fn main() {
         writers.push(handle);
     });
 
-    // JoinHandles
+    /////////////////
+    // JoinHandles //
+    /////////////////
     readers
         .into_iter()
         .for_each(|handle| handle.join().unwrap());
